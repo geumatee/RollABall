c2085d83a2423f8394e51e3aa471a95301cd8096
Matee Jariyavorakij <matee@hlpth.com>
2016-08-14 23:36:13 +0700

    First Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CameraController : MonoBehaviour {
+
+    private List<GameObject> players;
+    private Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        players = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
+        transform.position = new Vector3(AveragePlayersPostionX(), transform.position.y, transform.position.z);
+        CalculateOrthographicSize();
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(AveragePlayersPostionX(), transform.position.y, transform.position.z);
+        CalculateOrthographicSize();
+    }
+
+    private float AveragePlayersPostionX()
+    {
+        return players.Average(p => p.transform.transform.position.x);
+    }
+
+    private void CalculateOrthographicSize()
+    {
+        camera.orthographicSize = ((players.Max(p => p.transform.transform.position.x) + 1) - (players.Min(p => p.transform.transform.position.x) - 1)) / camera.aspect / 2f;
+        if (camera.orthographicSize < 3) camera.orthographicSize = 3;
+    }
+}
